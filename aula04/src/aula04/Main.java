2b3e7f641dc87b45f89bec3b690ec0110ab28df8
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-06-30 15:56:56 -0300

    aula 04

---
+++ aula04/src/aula04/Main.java
@@ -0,0 +1,10 @@
+package aula04;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Caneta caneta1 = new Caneta("BIC", 0.5f, false, "Azul");
+
+        caneta1.status();
+    }
+}
