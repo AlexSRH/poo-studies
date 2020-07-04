7e2f230b85a211e6f4ad7081335a83004bced9d6
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-07-04 08:34:19 -0300

    aula 06 denovo

--- aula06/src/aula06/Main.java
+++ aula06/src/aula06/Main.java
@@ -3,6 +3,14 @@ package aula06;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    ControleRemoto controleRemoto = new ControleRemoto();
+
+	    controleRemoto.ligar();
+	    controleRemoto.maisVolume();
+	    controleRemoto.maisVolume();
+	    controleRemoto.maisVolume();
+	    controleRemoto.maisVolume();
+	    controleRemoto.maisVolume();
+	    controleRemoto.abrirMenu();
     }
 }
